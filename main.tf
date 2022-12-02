5a89915d42e89b21c011263cd2fae38eb4d450d1
Matthew Gillespie <mgillespie+GIT@aviatrix.com>
2022-12-02 14:53:45 -0500

    adding another vpc

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ module "vpc" {
     Version = "v1.1.5"
   }
 }
+
+module "vpc2" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins_testbuild2"
+  cidr = "172.18.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1a"]
+  private_subnets = ["172.18.3.0/24", "172.18.4.0/24"]
+  public_subnets  = ["172.18.1.0/24", "172.18.2.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "jenkins_testbuild1"
+    Version = "v1.1.5"
+  }
+}
